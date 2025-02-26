192d7ca4cb255ca99836254a47a72612477acc92
Marsudi <marsudi.marsudi@nawatech.co>
2025-02-26 20:30:23 +0700

    code: 1

--- main.go
+++ main.go
@@ -5,14 +5,27 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
 	app := fiber.New()
 
+	// Middleware untuk mencatat log setiap request
+	app.Use(logger.New(logger.Config{
+		Format:     "[${time}] ${ip} - ${method} ${path} ${status} ${latency} - ${ua}\n",
+		TimeFormat: "2006-01-02 15:04:05",
+		TimeZone:   "Asia/Jakarta",
+	}))
+
 	// Route utama
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, Fiber!")
+	})
+
+	// Route lain untuk testing
+	app.Get("/test", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{"message": "This is a test route"})
 	})
 
 	// Port dari environment variable atau default 8080
